fbe7adefe96dc882d26c826c6a53a4afb2410012
Lorenz Steinert <lorenz@steinerts.de>
2020-11-22 16:36:05 +0100

    collapse namespace and add const

--- gslcpp/src/math.cpp
+++ gslcpp/src/math.cpp
@@ -4,68 +4,87 @@
 // gsl
 #include "gsl/gsl_math.h"
 
-namespace gslcpp
+namespace gslcpp::math
 {
-    namespace math
+    bool isnan(const double x)
     {
-        bool isnan(const double x)
-        {
-            return gsl_isnan(x);
-        }
+        return gsl_isnan(x) != 0;
+    }
 
-        int isinf(const double x)
-        {
-            return gsl_isinf(x);
-        }
+    int isinf(const double x)
+    {
+        return gsl_isinf(x);
+    }
 
-        bool finite(const double x)
-        {
-            return gsl_finite(x);
-        }
+    bool finite(const double x)
+    {
+        return gsl_finite(x) != 0;
+    }
 
-        double log1p(const double x)
-        {
-            return gsl_log1p(x);
-        }
+    double log1p(const double x)
+    {
+        return gsl_log1p(x);
+    }
 
-        double expm1(const double x)
-        {
-            return gsl_expm1(x);
-        }
+    double expm1(const double x)
+    {
+        return gsl_expm1(x);
+    }
 
-        double hypot(const double x, const double y)
-        {
-            return gsl_hypot(x, y);
-        }
+    double hypot(const double x, const double y)
+    {
+        return gsl_hypot(x, y);
+    }
 
-        double hypot(const double x, const double y, const double z)
-        {
-            return gsl_hypot3(x, y, z);
-        }
+    double hypot(const double x, const double y, const double z)
+    {
+        return gsl_hypot3(x, y, z);
+    }
 
-        double acosh(const double x)
-        {
-            return gsl_acosh(x);
-        }
+    double acosh(const double x)
+    {
+        return gsl_acosh(x);
+    }
 
-        double asinh(const double x)
-        {
-            return gsl_asinh(x);
-        }
+    double asinh(const double x)
+    {
+        return gsl_asinh(x);
+    }
 
-        double atanh(const double x)
-        {
-            return gsl_atanh(x);
-        }
+    double atanh(const double x)
+    {
+        return gsl_atanh(x);
+    }
 
-        double ldexp(double x, int e)
-        {
-            return gsl_ldexp(x, e);
-        }
+    double ldexp(const double x, const int e)
+    {
+        return gsl_ldexp(x, e);
+    }
 
-        double frexp(double x, int* e)
-        {
-            return gsl_frexp(x, e);
+    double frexp(const double x, int* e)
+    {
+        return gsl_frexp(x, e);
+    }
+
+    double pow(const double x, const int n)
+    {
+        return gsl_pow_int(x, n);
+    }
+
+    double pow(const double x, const unsigned int n)
+    {
+        return gsl_pow_uint(x, n);
+    }
+
+    std::strong_ordering fcmp(const double x, const double y, const double epsilon)
+    {
+        int res = gsl_fcmp(x, y, epsilon);
+        if (res == 0) {
+            return std::strong_ordering::equal;
         }
-    } // namespace math
-} // namespace gslcpp
+        if (res == -1) {
+            return std::strong_ordering::less;
+        }
+        return std::strong_ordering::greater;
+    }
+} // namespace gslcpp::math
